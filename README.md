632ca56be65fa0dec6bc72b7bac02ef46efff795
d.filyukov <d.filyukov@lucky-team.pro>
2022-06-20 21:45:16 +0200

    Step3: Finished

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Blazelip/backend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Blazelip/backend-project-lvl2/actions)
+
+#### Initial
+
+[![asciicast](https://asciinema.org/a/LCSDAgcpsiX8Oq6f7cN3AJvli.svg)](https://asciinema.org/a/LCSDAgcpsiX8Oq6f7cN3AJvli)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,76 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
+
+const findDiff = (obj1, obj2) => {
+  const keys = _.sortBy(Object.keys({...obj1, ...obj2}));
+
+  const result = keys.map((key) => {
+    if (!obj2.hasOwnProperty(key)) {
+      return {
+        name: key,
+        value: obj1[key],
+        status: 'deleted'
+      };
+    }
+
+    if (!obj1.hasOwnProperty(key)) {
+      return {
+        name: key,
+        value: obj2[key],
+        status: 'added'
+      };
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return {
+        name: key,
+        value1: obj1[key],
+        value2: obj2[key],
+        status: 'modified'
+      };
+    }
+
+    return {
+      name: key,
+      value: obj1[key],
+      status: 'unchanged'
+    };
+
+  });
+
+  return result;
+};
 
 export default (filepath1, filepath2) => {
   const path1 = path.resolve(process.cwd(), filepath1);
   const path2 = path.resolve(process.cwd(), filepath2);
 
-  const fileContent1 = JSON.parse(fs.readFileSync(path1, {encoding:'utf8'}));
-  const fileContent2 = JSON.parse(fs.readFileSync(path2, {encoding:'utf8'}));
-  return fileContent1;
+  const data1 = JSON.parse(fs.readFileSync(path1, { encoding:'utf8' }));
+  const data2 = JSON.parse(fs.readFileSync(path2, { encoding:'utf8' }));
+  
+  const diff = findDiff(data1, data2);
+
+  const result = {};
+
+  for (const prop of diff) {
+    switch (prop.status) {
+      case 'deleted':
+        result[`- ${prop.name}`] = prop.value;
+        break;
+      case 'added':
+        result[`+ ${prop.name}`] = prop.value;
+        break;
+      case 'modified':
+        result[`- ${prop.name}`] = prop.value1;
+        result[`+ ${prop.name}`] = prop.value2;
+        break;
+      case 'unchanged':
+        result[prop.name] = prop.value;
+        break;
+    }
+  }
+
+  return JSON.stringify(result);
 };
+
