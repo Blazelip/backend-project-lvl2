52cd742539602a28bb3c8a762f90582b1e5ca4ed
d.filyukov <d.filyukov@lucky-team.pro>
2022-07-20 17:39:52 +0200

    Added default value to gendiff func

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ const findDiff = (obj1, obj2) => {
   return result;
 };
 
-export default (filepath1, filepath2, format) => {
+export default (filepath1, filepath2, format = 'stylish') => {
   const content1 = getFileContent(filepath1);
   const content2 = getFileContent(filepath2);
 
